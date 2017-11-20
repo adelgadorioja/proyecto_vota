813bc9afd48914208ca8ffb7c90236b64c6d2a18
Álvaro <adelgado.sab@gmail.com>
2017-11-20 19:09:12 +0100

    Primeros pasos

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PROYECT VOTA</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="reset.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+
+	<div id="contenedorIndex">
+		<div id="presentacion">
+			<h1>¡PROYECTO VOTA!</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam elementum massa non rhoncus iaculis. Pellentesque pellentesque, diam ac interdum ullamcorper, mauris lorem sodales nisl, eget ullamcorper risus nisl non quam. Nulla vitae mauris urna. Cras congue tincidunt ornare. Nunc hendrerit ipsum quis pulvinar molestie. Integer tempus, purus sed efficitur tristique, justo nibh laoreet est, fermentum scelerisque purus arcu id metus. Mauris pulvinar vel quam at pretium. Vivamus dapibus odio vitae justo placerat malesuada.</p>
+		</div>
+		<form id="formularioLogin">
+			<label for="usuario">Usuario</label>
+			<input type="text" id="usuario" name="usuario">
+			<label for="contrasena">Contraseña</label>
+			<input type="text" id="contrasena" name="contrasena">
+			<input type="submit" id="botonAceptar" name="aceptar" value="Aceptar">
+		</form>
+	</div>
+
+</body>
+</html>

58d218d0bc1a183a5fc3f10ca48c36f3f92e0c62
adelgadorioja <32141286+adelgadorioja@users.noreply.github.com>
2017-11-22 17:52:43 +0100

    Modificado index

--- index.php
+++ index.php
@@ -10,11 +10,7 @@
 <body>
 
 	<div id="contenedorIndex">
-		<div id="presentacion">
-			<h1>¡PROYECTO VOTA!</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam elementum massa non rhoncus iaculis. Pellentesque pellentesque, diam ac interdum ullamcorper, mauris lorem sodales nisl, eget ullamcorper risus nisl non quam. Nulla vitae mauris urna. Cras congue tincidunt ornare. Nunc hendrerit ipsum quis pulvinar molestie. Integer tempus, purus sed efficitur tristique, justo nibh laoreet est, fermentum scelerisque purus arcu id metus. Mauris pulvinar vel quam at pretium. Vivamus dapibus odio vitae justo placerat malesuada.</p>
-		</div>
-		<form id="formularioLogin">
+		<form id="formularioLogin" action="PHP/validacionUsuario.php" method="POST">
 			<label for="usuario">Usuario</label>
 			<input type="text" id="usuario" name="usuario">
 			<label for="contrasena">Contraseña</label>
